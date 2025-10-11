cd739e9f54588c16283a9a2e2cd189d93b4a7b74
Johannes Schmid <johannes@schmid-compnay.de>
2025-10-11 12:36:32 +0200

    added onlyoffice template

---
+++ templates/onlyoffice/service.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+SERVICE_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+cd $SERVICE_DIR
+
+# CORE
+source ../.env
+source ../$CORE_DIR_NAME/core.sh
+
+# VARIABLES
+set -o allexport
+# set variables for docker or other services here
+set +o allexport
+
+# COMMANDS
+
+commands+=([example]=":Start onlyoffice example")
+cmd_example() {
+  docker compose exec onlyoffice sudo supervisorctl start ds:example
+}
+
+# ATTACHMENTS
+
+# Setup function that is called before the docker up command
+# att_setup() {
+#   echo "Setting up..."
+# }
+
+# Configure function that is called before the docker up, start and restart commands
+# att_configure() {
+#   echo "Configuring..."
+# }
+
+# MAIN
+main "$@"
